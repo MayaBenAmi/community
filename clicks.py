a198301b6da260979738512e0c5b591027c4607b
noga <noga.hosi@gmail.com>
2023-09-20 15:03:23 +0300

    THE INSTRUCTIONS BUTTON WORKS

--- clicks.py
+++ clicks.py
@@ -1,22 +1,25 @@
 import pygame
 import consts
 
+pygame.init()
 
-mouse_x = pygame.mouse.get_pos()[0]
-mouse_y = pygame.mouse.get_pos()[1]
+
+# mouse_x = pygame.mouse.get_pos()[0]
+# mouse_y = pygame.mouse.get_pos()[1]
 
 
 def check_x(mouse_x):
-    min_x = consts.INSTRUCTIONS_BUTTON_LOCATION[0] - consts.BUTTON_DIAMETER / 2
-    max_x = consts.INSTRUCTIONS_BUTTON_LOCATION[0] + consts.BUTTON_DIAMETER / 2
+    min_x = consts.INSTRUCTIONS_BUTTON_LOCATION[0]
+    max_x = consts.INSTRUCTIONS_BUTTON_LOCATION[0] + consts.BUTTON_DIAMETER
     if min_x <= mouse_x <= max_x:
         return True
     else:
         return False
 
+
 def check_instructions_y(mouse_y):
-    min_y = consts.INSTRUCTIONS_BUTTON_LOCATION[1] - consts.BUTTON_DIAMETER / 2
-    max_y = consts.INSTRUCTIONS_BUTTON_LOCATION[1] + consts.BUTTON_DIAMETER / 2
+    min_y = consts.INSTRUCTIONS_BUTTON_LOCATION[0]
+    max_y = consts.INSTRUCTIONS_BUTTON_LOCATION[0] + consts.BUTTON_DIAMETER
     if min_y <= mouse_y <= max_y:
         return True
     else:
@@ -38,5 +41,4 @@ def check_flag_y(mouse_y):
         return True
     else:
         return False
-
 
--- our_main.py
+++ our_main.py
@@ -9,13 +9,19 @@ running = True
 while running:
     draw_game()
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+
         if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x = pygame.mouse.get_pos()[0]
             if check_x(mouse_x):
+                mouse_y = pygame.mouse.get_pos()[1]
                 if check_instructions_y(mouse_y):
                     open_instructions()
-                # if check_bubbles_y(mouse_y:
+                # if check_bubbles_y(mouse_y):
                 #     # open bubbles game
                 # if check_flag_y(mouse_y):
                 #     # open the flag game
-        if event.type == pygame.QUIT:
-            running = False
+    pygame.display.flip()
