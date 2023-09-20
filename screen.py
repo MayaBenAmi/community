63a538836ed3b0b9d2fc95f1c300d85ad37cdcdd
noga <noga.hosi@gmail.com>
2023-09-20 11:33:56 +0300

    open screen and consts modules

--- screen.py
+++ screen.py
@@ -7,25 +7,25 @@ screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 def draw_bubbles_button(bubbles_img):
     bubbles_button = pygame.image.load(bubbles_img)
     sized_bubbles = pygame.transform.scale(bubbles_button, (consts.BUTTON_DIAMETER, consts.BUTTON_DIAMETER))
-    screen.blit(sized_bubbles, (50, 2 * consts.WINDOW_HEIGHT / 4))
+    screen.blit(sized_bubbles, (50, 500))
 
 
 def draw_flag_button(flag_img):
     flag_button = pygame.image.load(flag_img)
     sized_flag = pygame.transform.scale(flag_button, (consts.BUTTON_DIAMETER, consts.BUTTON_DIAMETER))
-    screen.blit(sized_flag, (50, 2 * consts.WINDOW_HEIGHT / 4))
+    screen.blit(sized_flag, (50, 300))
 
 
 def draw_instructions_button(instructions_img):
     instructions_button = pygame.image.load(instructions_img)
     sized_instructions = pygame.transform.scale(instructions_button, (consts.BUTTON_DIAMETER, consts.BUTTON_DIAMETER))
-    screen.blit(sized_instructions, (50, consts.WINDOW_HEIGHT / 4))
+    screen.blit(sized_instructions, (50, 100))
 
 
 def draw_happy_bunny(happy_bunny_img):
     happy_bunny = pygame.image.load(happy_bunny_img)
     sized_happy_bunny = pygame.transform.scale(happy_bunny, (consts.BUNNY_WIDTH, consts.BUNNY_HIGHT))
-    screen.blit(sized_happy_bunny, (consts.WINDOW_WIDTH / 2, consts.WINDOW_HEIGHT / 2))
+    screen.blit(sized_happy_bunny, (350, 200))
 
 
 def draw_sad_bunny(sad_bunny_img):
@@ -36,9 +36,9 @@ def draw_sad_bunny(sad_bunny_img):
 
 def draw_bushes(grass_img):
     grass = pygame.image.load(grass_img)
-    sized_grass = pygame.transform.scale(grass, (50, 30))
-    screen.blit(sized_grass, (consts.WINDOW_WIDTH / 2 - 60, consts.WINDOW_HEIGHT / 2))
-    screen.blit(sized_grass, (consts.WINDOW_WIDTH / 2 + 60, consts.WINDOW_HEIGHT / 2))
+    sized_grass = pygame.transform.scale(grass, (100, 50))
+    screen.blit(sized_grass, (300, 500))
+    screen.blit(sized_grass, (600, 500))
 
 
 
@@ -51,9 +51,14 @@ def draw_game():
     draw_instructions_button("instructionsdot.png")
     draw_happy_bunny("happy_bunny.png")
     draw_bushes("grass.png")
-
-
     pygame.display.flip()
 
+
+running = True
+while running:
+    draw_game()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
 draw_game()
